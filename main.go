3ac058f1b83256ab661e490cfa1241eee4c838c0
rajkumarGosavi <princegosavi12@gmail.com>
2021-03-25 09:32:47 +0530

    moved server initialization to main

--- main.go
+++ main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"log"
 	"net"
+	"userMocker/core"
 	"userMocker/server"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":0")
+	lis, err := net.Listen("tcp", ":9090")
+	port := uint32(lis.Addr().(*net.TCPAddr).Port)
+
 	if err != nil {
-		log.Fatalf("Failed to listen to %d %v", lis.Addr().(*net.TCPAddr).Port, err)
+		log.Fatalf("Failed to listen to %d %v", port, err)
 	}
-	err = server.InitServer(&lis)
+	s, err := server.InitServer(&lis)
 	if err != nil {
 		log.Fatalf("Failed to initialize GRPC server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	core.RegisterUserGetterServer(grpcServer, &s)
+	core.RegisterUsersGetterServer(grpcServer, &s)
+	log.Printf("Initialized GRPC Server, listening on %d ", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve grpc %v", err)
 	}
 }
